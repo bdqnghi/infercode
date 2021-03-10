8363860a9c8f6157b2238acc159e9c5a2d673755
Yijun Yu <y.yu@open.ac.uk>
2021-03-10 22:04:33 +0000

    generate node_types from node-types.json, not from grammar.json

--- utils/node_types_grammar.sh
+++ utils/node_types_grammar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd $HOME/node_modules > /dev/null
-find . -name node-types.json | while read f; do
+find . -name grammar.json | while read f; do
 	lang=$(basename $(dirname $(dirname $f)) | sed -e 's/tree-sitter-//g')
-	cat $f | jq '.'| grep '"type": "' | awk '{print $2}'| sed -e 's/,//g' -e 's/"//g' | sort | uniq | while read l; do
+	cat $f | jq '.'| grep '"name": "' | awk '{print $2}'| sed -e 's/,//g' -e 's/"//g' | sort | uniq | while read l; do
 	md=$(echo $l | md5sum)
 	echo $md,$l | sed -e 's/ -//g'
-done > node_types_$lang.txt
+done > node_types_grammar_$lang.txt
 done
 cd - > /dev/null
 
