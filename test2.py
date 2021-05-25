85a9f46ceee62fd195e3beda6a6e0b686138f606
Quoc Nghi <bdqnghi@gmail.com>
2021-05-25 11:40:56 +0700

    delete some files

--- test2.py
+++
@@ -1,7 +0,0 @@
-import pickle
-tree_path = "java-small-test/buckets-all-training.pkl"
-data = pickle.load(open(tree_path, "rb"))
-for k, v in data.items():
-    print("Key", k)
-    print("Len v", len(v))
-    print(v)
--- test_2.py
+++
@@ -1,4 +0,0 @@
-from dpu_utils.codeutils import split_identifier_into_parts
-
-
-print(split_identifier_into_parts("eatRelationalExpression"))
