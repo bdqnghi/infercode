4dd5807569b20a3c58e681862834d4c978057e27
Quoc Nghi <bdqnghi@gmail.com>
2021-06-10 19:39:34 +0700

    update

---
+++ generate_srcml_pkl.py
@@ -0,0 +1,46 @@
+import os
+# from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor as WorkerExecutor
+import copy
+import sys
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--worker", default=6, type=int, help="Num worker")
+parser.add_argument("--input_path", default="java-small/training", type=str, help="Input path")
+parser.add_argument("--output_path", default="java-small_pkl/training", type=str, help="Output path")
+
+args = parser.parse_args()
+
+
+# docker run --cpus=10 --rm -v $(pwd):/e --entrypoint pkl -it yijun/fast OJ_raw_train_test_val/val OJ_pkl_train_test_val/val_2
+
+def generate_folder_pb(src_path, tgt_path, worker):
+    cmd = "docker run --cpus=" + str(worker) +  " --rm -v $(pwd):/e -it yijun/fast bash pkl " + src_path + " " + tgt_path
+    # cmd = "docker run --rm -v $(pwd):/e -it yijun/fast -p " + src_path + " " + tgt_path
+    print(cmd)
+    os.system(cmd)
+
+def main():
+    worker = args.worker
+    input_path = args.input_path
+    output_path = args.output_path
+
+    Path(output_path).mkdir(parents=True, exist_ok=True)
+
+
+    for subdir, dirs, files in os.walk(input_path):
+        for project in dirs:
+            raw_dir_path = os.path.join(subdir, project)
+            target_path = os.path.join(output_path, project)
+            Path(target_path).mkdir(parents=True, exist_ok=True)
+
+            generate_folder_pb(raw_dir_path, target_path, worker)
+
+
+
+            # print(fbs_path)
+
+if __name__ == "__main__":
+    main()
