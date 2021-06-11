2ad75a493e4351d64a9d528f3f20184913777610
Quoc Nghi <bdqnghi@gmail.com>
2021-06-11 18:07:13 +0700

    update

--- script/preprocess_data.py
+++ script/preprocess_data.py
@@ -2,34 +2,25 @@ import sys
 from pathlib import Path
 # To import upper level modules
 sys.path.append(str(Path('.').absolute().parent))
-from utils.data.data_processor.treesitter_data_processor import TreeSitterDataProcessor
+import random
+import pickle
+from utils.data.tree_processor import TreeProcessor
+import argument_parser
+# import utils.network.treecaps_2 as network
+import os
+import re
+import time
+from bidict import bidict
+import copy
+import numpy as np
+from utils import evaluation
 
 
-import argparse
-
-def parse_arguments(): 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', default="../java-small-test/training")
-    parser.add_argument('--node_type_vocab_path',default="../vocab/treesitter/node_type/node_types_c_java_cpp_c-sharp_rust.txt")
-    parser.add_argument('--node_token_vocab_path', default="../vocab/treesitter/node_token/token.txt")
-    parser.add_argument('--subtree_vocab_path', default="../subtrees/subtrees.txt")
-    # parser.add_argument('--parser', type=str, default="treesitter", help="treesitter")
-    opt = parser.parse_args(),
-    return opt
 
 def main(opt):
-    data_path = opt.data_path
-    node_type_vocab_path = opt.node_type_vocab_path
-    node_token_vocab_path = opt.node_token_vocab_path
-    subtree_vocab_path = opt.subtree_vocab_path
-    # parser = opt.parser
-    
-    
-    # if parser == "treesitter":
-    processor = TreeSitterDataProcessor(node_type_vocab_path, node_token_vocab_path, subtree_vocab_path, data_path)
-   
+    tree_processor = TreeProcessor(opt)
+    tree_processor.process_data()
 
 if __name__ == "__main__":
-    opt = parse_arguments()
-    main(opt[0])
-   
+    opt = argument_parser.parse_arguments()
+    main(opt)
