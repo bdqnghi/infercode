ebce90735f557e6baa0e94993c5ec88067d263e7
Quoc Nghi <bdqnghi@gmail.com>
2021-08-17 16:44:36 +0700

    update

--- infercode/script/build_vocab_from_file.py
+++ infercode/script/build_vocab_from_file.py
@@ -9,10 +9,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--text_file', type=str, required=True)
 parser.add_argument('--model_file', type=str, required=True)
 parser.add_argument('--model_type', type=str, default="word")
+parser.add_argument('--vocab_size', type=int, default=500000)
 
 def main(opt):
 
-    vocab = Vocabulary(10000000)
+    vocab = Vocabulary(opt.vocab_size)
     vocab.create_vocabulary_from_file(sp_text_file=opt.text_file, model_filename=opt.model_file, model_type=opt.model_type)        
 
 # python3 extract_token_vocab.py --data_path ../../datasets/OJ_raw_small/ --node_token_vocab_model_prefix OJ_raw_token
