dcb60f84be9ef21dfa9d5539b77deacba9168762
Yijun Yu <yijun.yu@huawei.com>
2021-08-22 18:56:44 +0800

    fixing a typo

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires=[
 
 setup(
   name = 'infercode',
-  version = '0.0.17',
+  version = '0.0.18',
   py_modules = ['infercode'],
   description = 'Map any code snippet into vector',
   author = 'Nghi D. Q. Bui and Yijun Yu',
@@ -35,7 +35,7 @@ setup(
   ],
   package_dir={"infercode": "infercode"},
   packages=find_packages(where=".", exclude=["tests", "logo", "datasets"]),
-  package_data={'infercode': ['configs/*.ini', 'sentencepiece_vocab/*', 'sentencepiece_vocab/node_types/*' , 'sentencepiece_vocab/subtrees/*', 'sentencepience_vocab/tokens/*' ]},
+  package_data={'infercode': ['configs/*.ini', 'sentencepiece_vocab/*', 'sentencepiece_vocab/node_types/*' , 'sentencepiece_vocab/subtrees/*', 'sentencepiece_vocab/tokens/*' ]},
   install_requires=install_requires,
   include_package_data=True,
 )
