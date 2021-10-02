9c220f0dd3be5a835907ef99cee03414aefdb19d
Yijun Yu <y.yu@open.ac.uk>
2021-10-02 10:27:25 +0000

    remove the warnings

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
-from infercode.client.infercode_client import InferCodeClient
 import os
-import logging
-logging.basicConfig(level=logging.INFO)
-
 # Change from -1 to 0 to enable GPU
 os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import warnings
+warnings.filterwarnings("ignore")
+import tensorflow as tf
+import logging
+logging.getLogger('tensorflow').disabled = True
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR) 
+try:
+    # Disable all GPUS
+    tf.config.set_visible_devices([], 'GPU')
+    visible_devices = tf.config.get_visible_devices()
+    for device in visible_devices:
+        assert device.device_type != 'GPU'
+except:
+    # Invalid device or cannot modify virtual devices once initialized.
+    pass
+from infercode.client.infercode_client import InferCodeClient
 
 infercode = InferCodeClient(language="c")
 infercode.init_from_config()
